405a1c99476e7f908ee0ce22bdd5b774ab2ea641
Nicolas Even <n-e@users.noreply.github.com>
2017-10-11 00:09:08 +0200

    Improved 'make test' output display

--- test.sh
+++ test.sh
@@ -6,4 +6,9 @@ cut -d '$' -f1 tests.txt > tmp/in
 cut -d '$' -f2 tests.txt > tmp/expected
 ./cnitize < tmp/in > tmp/out
 
-diff -u tmp/expected tmp/out
+diff -y --suppress-common-lines tmp/out tmp/expected 
+
+FAIL=`diff --suppress-common-lines --speed-large-files -y tmp/out tmp/expected |wc -l |sed 's/[^0-9]//g'`
+ALL=`cat tmp/expected|wc -l |sed 's/[^0-9]//g'`
+let "SUCCESS = $ALL - $FAIL"
+echo "Succeeded: $SUCCESS/$ALL"
